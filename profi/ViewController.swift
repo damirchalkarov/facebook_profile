13540fe9051e19cdbfc75f27aa6ed050c9b178a9
damiru2 <chalkarovd11@gmail.com>
2024-10-13 15:56:07 +0500

    add scroll

--- profi/ViewController.swift
+++ profi/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var ellipsisButton: UIButton!
+    
+    @IBOutlet weak var friend1Image: UIImageView!
+    @IBOutlet weak var friend2Image: UIImageView!
+    @IBOutlet weak var friend3Image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        profileImage.layer.cornerRadius = 45
+        ellipsisButton.layer.cornerRadius = 8
+        
+        //        friend1Image.layer.shadowColor = UIColor.black.cgColor
+        //            friend1Image.layer.shadowOpacity = 0.7
+        //            friend1Image.layer.shadowOffset = CGSize(width: 5, height: 5)
+        //            friend1Image.layer.shadowRadius = 10
+        //            friend1Image.layer.masksToBounds = false
+        //        
+        //
+        //        friend2Image.layer.shadowColor = UIColor.black.cgColor
+        //            friend2Image.layer.shadowOpacity = 0.7
+        //            friend2Image.layer.shadowOffset = CGSize(width: 5, height: 5)
+        //           friend2Image.layer.shadowRadius = 10
+        //            friend2Image.layer.masksToBounds = false
+        //      
+        //        friend3Image.layer.shadowColor = UIColor.black.cgColor
+        //            friend3Image.layer.shadowOpacity = 0.7
+        //            friend3Image.layer.shadowOffset = CGSize(width: 5, height: 5)
+        //            friend3Image.layer.shadowRadius = 10
+        //            friend3Image.layer.masksToBounds = false
+        //            
+        //    }
+        
+        
     }
-
-
 }
-
